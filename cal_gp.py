9a41daa3cd5ada0f46832b207a64538992b0065c
comsavvy <olusolaogundepo@gmail.com>
2020-05-22 20:40:37 +0100

    Addition of DataFrame and Countplot

--- cal_gp.py
+++ cal_gp.py
@@ -1,12 +1,34 @@
-import matplotlib.pyplot as plt
+
+"""
+This module can be used to calculate the total CGPA of a student per each semester
+
+"""
+# Importing the necessary Libraries to use
 import pandas as pd
-import numpy as np
+import seaborn as sns
 class G4:
+    """
+    This contains alot of fuctions such as:
+    o __advice
+    o _Score
+    o start (This is where other methods are executed)
+    o __dataFrame(This will create the DataFrame of all the data you've inputted)    
+    o showdataframe (This allows you to see the DataFrame of the data)
+    
+    Out of all the listed functions, those that're mearnt to be call are:
+    - start
+    - Showdataframe
+    
+    """
     def __init__(self):
-        self.gradde= {'Grade': [],
+        self.holder= {'Grade': [],
              'GradePoint': [],
              'Unit': []}
-    def advice(self, gp):
+    def __advice(self, gp):
+        """
+        This is going to return Advice on the performance of the Student
+        
+        """
         if gp >= 4.5:
             print('Weldone, You Are In First Class 👍')
         elif gp >= 3.5:
@@ -16,81 +38,100 @@ class G4:
         elif gp >= 2.39:
             print('Work Hard, You are in Third Class')
         else:
-            print('You pass through school')
+            print('Certificate of attendance')
 
-    def score(self, total_unit, total_courses):
-            result= 0
-            course= 0
-            unit_counter=0
-            increment= 1
-            if total_unit != 0:
-                try:
-                    while increment <= total_courses:
-                        grade= (input(f"Grade_{increment}: ").upper())
-                        self.gradde['Grade'].append(grade)
-                        unit= int(input(f"Course_{increment} unit: "))
-                        self.gradde['Unit'].append(unit)
-                        print("\n")
-                        if grade == 'A':
-                            point= 5
-                            self.gradde['GradePoint'].append(point)
-                            course= point * unit
-                        elif grade == 'B':
-                            point= 4
-                            self.gradde['GradePoint'].append(point)
-                            course= point * unit
-                        elif grade == 'C':
-                            point= 3
-                            self.gradde['GradePoint'].append(point)
-                            course= point * unit 
-                        elif grade == 'D':
-                            point= 2
-                            self.gradde['GradePoint'].append(point)
-                            course= point * unit
-                        elif grade == 'E':
-                            point= 1
-                            self.gradde['GradePoint'].append(point)
-                            course= point * unit
-                        elif grade == 'F':
-                            point= 0
-                            self.gradde['GradePoint'].append(point)
-                            course= point
-                        else:
-                            print('Invalid Grade')
-                            break
-                        increment+= 1  
-                        unit_counter += unit
-                        result += course
-                    if total_unit == unit_counter:
-                        gp = result / total_unit
-                        print(f"Grade: {round(gp, 3)}")
-                        self.advice(gp)    
+    def __score(self, total_unit, total_courses):
+        """
+        You don't need to call this function, it a special method to be call only 
+        by `Start' method
+        
+        """
+        result= 0
+        course= 0
+        unit_counter=0
+        increment= 1
+        if total_unit != 0:
+            try:
+                while increment <= total_courses:
+                    grade= (input(f"Grade_{increment}: ").upper())
+                    self.holder['Grade'].append(grade)
+                    unit= int(input(f"Course_{increment} unit: "))
+                    self.holder['Unit'].append(unit)
+                    print("\n")
+                    if grade == 'A':
+                        point= 5
+                        self.holder['GradePoint'].append(point)
+                        course= point * unit
+                    elif grade == 'B':
+                        point= 4
+                        self.holder['GradePoint'].append(point)
+                        course= point * unit
+                    elif grade == 'C':
+                        point= 3
+                        self.holder['GradePoint'].append(point)
+                        course= point * unit 
+                    elif grade == 'D':
+                        point= 2
+                        self.holder['GradePoint'].append(point)
+                        course= point * unit
+                    elif grade == 'E':
+                        point= 1
+                        self.holder['GradePoint'].append(point)
+                        course= point * unit
+                    elif grade == 'F':
+                        point= 0
+                        self.holder['GradePoint'].append(point)
+                        course= point
                     else:
-                        print('Unit not balance')
-                except ValueError:
-                    print('Wrong input!')
-                except UnboundLocalError:
-                    print('Check Input')
-                except ZeroDivisionError:
-                    print('Undefine')
-            else:
-                print('Total Unit can not be zero!')
+                        print('Invalid Grade')
+                        break
+                    increment+= 1  
+                    unit_counter += unit
+                    result += course
+                if total_unit == unit_counter:
+                    gp = result / total_unit
+                    print(f"Grade: {round(gp, 3)}")
+                    self.__advice(gp)    
+                else:
+                    print('Unit not balance')
+            except ValueError:
+                print('Wrong input!')
+            except UnboundLocalError:
+                print('Check Input')
+            except ZeroDivisionError:
+                print('Undefine')
+        else:
+            print('Total Unit can not be zero!')
 
-    def cont(self):
+    def start(self):
         try:
             total_unit= int(input("Total Unit: "))
             total_courses= int(input("Total Courses: "))
             print("\n")
-            self.score(total_unit, total_courses)
+            self.__score(total_unit, total_courses)
         except ValueError:
             print('Value Error')
             
-    def dataFrame(self):
-        df= pd.DataFrame(self.gradde)
+    def __dataFrame(self):
+        """
+        You don't need to call this function it going to generate dataframe
+        for showdataframe method
+        """
+        df= pd.DataFrame(self.holder)
         return df
     
-    def showFrame(self, data):
-        if data == 'Y':
-            self.dataFrame()
-        else: 
-            pass
+    def showdataframe(self, show= False):
+        """
+        This method allow you to view the DataFrame of your inputted data
+        """
+        df= self.__dataFrame()
+        return df
+        
+    def showcountplot(self):
+        """
+        This method is going to generate the number of instances a grade occur.
+        But plot will only generate when you close the program.
+        """
+        df= self.__dataFrame()
+        sns.countplot(data= df, x= 'Grade')
+        
--- gp.py
+++ gp.py
@@ -1,20 +1,39 @@
 from cal_gp import G4
 cg= G4()
+
+def showPlot(occurence):
+    if occurence== "Y" or occurence == "Yes":
+        cg.showcountplot()
+    else:
+        pass
+
+def request(data):
+    if data == "Y" or data == "Yes":
+        print(cg.showdataframe(True))
+    else:
+        pass
+    
 print('\t\tThis Is A Program For Calculating Total CGPA')          
-cg.cont()
+cg.start()
 data = input('Do you want to see the DataFrame of your Score: ').capitalize()
-cg.showFrame(data)
+request(data)
+occurence= input('Do you want to see the number of Instances of your Grades occurence in CountPlot? ').capitalize()
+showPlot(occurence)
+
 while True:
     try:    
         con_qu= int(input('''You have come to end of the program. 
 To calculate another CGPA press 1, To Quit the program press 0: '''))
         if con_qu == 1:
-            cg.cont()
-            data
+            cg.start()            
+            data = input('Do you want to see the DataFrame of your Score: ').capitalize()
+            request(data)
+            occurence= input('Do you want to see the number of Instances of your Grades occurence in CountPlot? ').lower()
+            showPlot(occurence)
         elif con_qu == 0:
             print('Quit🤞')
             break
         else:
-            print('Not allow')
+            print('ValueError!')
     except ValueError:
         print('Check the input')                
